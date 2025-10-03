a59d685f586f6e579d1126172e8f8ba4154a2814
RutySilveira <ruteneles@gmail.com>
2025-10-02 21:25:59 -0300

    Adicionando alterações nos exercícios

--- Secao2_JavaScript/calculadoraIMC/script.js
+++ Secao2_JavaScript/calculadoraIMC/script.js
@@ -1,32 +1,38 @@
 /*
-Abaixo de 17 Muito abaixo do peso
-Entre 17 e 18,49 Abaixo do peso
-Entre 18,5 e 24,99 Peso Normal
-Entre 25 e 29,99 Acima do peso
+Faixas de classificação do IMC:
+Abaixo de 17       -> Muito abaixo do peso
+Entre 17 e 18,49   -> Abaixo do peso
+Entre 18,5 e 24,99 -> Peso Normal
+Entre 25 e 29,99   -> Acima do peso
+Maior ou igual a 30 -> Obesidade
 */
 
-// peso / (altura * altura);
+// Fórmula do IMC: peso / (altura * altura);
 
-var peso;
-var altura;
-var imc;
-var resultado;
+var peso; // Variável para armazenar o peso informado
+var altura; // Variável para armazenar a altura informada
+var imc; // Variável para armazenar o cálculo do IMC
+var resultado; // Variável para manipular a <div> onde aparece o resultado
 
 function calcular(event) {
-  event.preventDefault();
+  event.preventDefault(); // Impede que o formulário recarregue a página
 
+  // Pega os valores digitados nos inputs
   peso = document.getElementById("peso").value;
   altura = document.getElementById("altura").value;
 
+  // Calcula o IMC (lembrando de converter para número automaticamente)
   imc = peso / (altura * altura);
 
+  // Pega a <div> onde o resultado vai ser exibido
   resultado = document.getElementById("resultado");
 
+  // Estrutura condicional para verificar em qual faixa o IMC se encaixa
   if (imc < 17) {
     resultado.innerHTML =
       "<br/> Seu resultado foi: " +
       imc.toFixed(2) +
-      "<br/> Cuidado voce está muito abaixo do peso!";
+      "<br/> Cuidado, você está muito abaixo do peso!";
   } else if (imc > 17 && imc <= 18.49) {
     resultado.innerHTML =
       "<br/> Seu resultado foi: " +
@@ -44,9 +50,12 @@ function calcular(event) {
       "<br/> Você está acima do peso!";
   } else if (imc >= 30) {
     resultado.innerHTML =
-      "<br/> Seu resultado foi: " + imc.toFixed(2) + "<br/> Cuidado Obesidade!";
+      "<br/> Seu resultado foi: " +
+      imc.toFixed(2) +
+      "<br/> Cuidado, obesidade!";
   }
 
+  // Limpa os campos de entrada após o cálculo
   document.getElementById("peso").value = "";
   document.getElementById("altura").value = "";
 }
